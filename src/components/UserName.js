9e14f3886d58dcd006edee9bb9214b20ca33b9df
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-27 15:11:33 -0500

    Synth component mounts after user name is submitted

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import '../scss/UserName.scss'
+import Synth from './Synth.js'
 
 export default class UserName extends Component {
     state = {
-        userName: ''
+        userName: '',
+        userNameSelected: false
     }
 
     onUserNameChange = e => {
@@ -14,23 +16,30 @@ export default class UserName extends Component {
     onUserNameSubmit = (e) => {
         e.preventDefault()
         console.log(`${this.state.userName} has joined the jam!`)
+        this.setState({userNameSelected: true})
     }
 
     render() {
         return (
-            <div className='userName'>
-                <form onSubmit={this.onUserNameSubmit}>
-                    <input
-                        type='text'
-                        placeholder='User Name'
-                        onChange={this.onUserNameChange}
-                        value={this.state.userName}
-                    />
-                    <input
-                        type='submit'
-                        value='Start Jamming!'
-                    />
-                </form>
+            <div>
+                {this.state.userNameSelected ? 
+                    <Synth userName = {this.state.userName}/>
+                : 
+                    <div className='userName'>
+                        <form onSubmit={this.onUserNameSubmit}>
+                            <input
+                                type='text'
+                                placeholder='User Name'
+                                onChange={this.onUserNameChange}
+                                value={this.state.userName}
+                            />
+                            <input
+                                type='submit'
+                                value='Start Jamming!'
+                            />
+                        </form>
+                    </div>
+                }
             </div>
         )
     }
